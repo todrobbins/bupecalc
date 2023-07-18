c18beae98c0926be99716ce1304e5b0ae22ad48d
Dazhong Xia <john.danger.xia@gmail.com>
2023-07-18 19:02:32 -0400

    Start with a simple table

---
+++ src/lib/Day.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let day;
+  export let startDate;
+  export let dose;
+  export let freq;
+  
+  let date = new Date(startDate);
+  date.setDate(startDate.getDate() + day);
+  $: totalDose = dose * freq
+</script>
+
+<div>
+  Day {day} ({date.toLocaleDateString()}): {dose} mg {freq}x/day (total dose {totalDose} mg)
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Day from '$lib/Day.svelte'
+
+  let doses = [
+    {dose: 25, freq: 2},
+    {dose: 25, freq: 3},
+    {dose: 50, freq: 2}
+  ];
+
+  let startDate = new Date()
+
+  // need to be able to override the doses by clicking on each element
+  // need to visualize
+  // need to add different drug types
+  // need to style
+</script>
+<h1>BupeCalc</h1>
+{#each doses as dose, i}
+  <Day day={i + 1} {startDate} dose={dose.dose} freq={dose.freq}/>
+{/each}
